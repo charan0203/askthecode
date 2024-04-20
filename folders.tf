4a38cfa98f19ea87cfc08d151cdfc14ca8b34860
Charan0203 <126796918+charan0203@users.noreply.github.com>
2024-04-20 16:10:05 +0530

    Update folders.tf

--- folders.tf
+++ folders.tf
@@ -1,9 +1,1 @@
-resource "google_folder" "network_folder" {
-  display_name = var.folder_display_name
-  parent          = var.root_node
- # parent       = "folders/${var.nbss_main_folder_id}"
-}
 
-data "google_folder" "my_folder_info" {
-   folder = google_folder.network_folder.id
-}
